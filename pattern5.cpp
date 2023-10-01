6176c108f3f38556fb19cbd344cbeba15c1ed9d1
Ayush Chaudhary <ayushchaudhary1702@gmail.com>
2023-10-02 03:36:52 +0530

    all patterns are modified

---
+++ pattern5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    int count=0;
+    for(int i=1;i<=n;i++){
+        count=i;
+        for(int j=1;j<=i;j++){
+            cout<<count;
+            count++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
